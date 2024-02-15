8d0fa691bf8ac96fd2125fea52b79faca91ada69
Arman <arman@gabric.ca>
2024-02-15 14:13:54 -0500

    the first one

---
+++ aginit.sh
@@ -0,0 +1,10 @@
+sudo apt-get update && sudo apt-get upgrade
+sudo apt install net-tools
+sudo apt install ipcalc
+sudo apt install zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sudo apt install git
+sudo apt -y install curl whois bind9-host mtr-tiny jq ipcalc grepcidr nmap ncat aha
+sudo apt install nmap
+sudo apt install speedtest-cli
+sudo apt install neofetch
