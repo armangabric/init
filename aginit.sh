265c293fe8a595616ec71c26f4b1656f87fa513d
Arman <arman@gabric.ca>
2024-02-27 23:37:08 -0500

    added -y flags to added logging to output.txt to see if binaries existed or not, in easier to read file.

--- aginit.sh
+++ aginit.sh
@@ -1,89 +1,89 @@
 sudo apt-get update && sudo apt-get upgrade
-sudo apt install net-tools
+sudo apt -y install net-tools
 #not one binary - multipl tools - check for actual binaries first then add the file checks
 
 FILE=/usr/bin/ipcalc
-if [ -f "$FILE" ]; then
-    echo "$FILE exists."
+if [ -f "$FILE" ]; then 
+    echo "$FILE exists." | tee -a output.txt
 else 
-    echo "$FILE does not exist."
-    sudo apt install ipcalc
+    echo "$FILE does not exist." | tee -a output.txt
+    sudo apt -y install ipcalc
 fi
 
 
 
 FILE=/usr/bin/zsh
-if [ -f "$FILE" ]; then
-    echo "$FILE exists."
+if [ -f "$FILE" ]; then 
+    echo "$FILE exists." | tee -a output.txt
 else 
-    echo "$FILE does not exist."
-    sudo apt install zsh
+    echo "$FILE does not exist." | tee -a output.txt
+    sudo apt -y install zsh
 fi
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 #check for actual binaries first then add the file checks
 
 FILE=/usr/bin/git
-if [ -f "$FILE" ]; then
-    echo "$FILE exists."
+if [ -f "$FILE" ]; then 
+    echo "$FILE exists." | tee -a output.txt
 else 
-    echo "$FILE does not exist."
-    sudo apt install git
+    echo "$FILE does not exist." | tee -a output.txt
+    sudo apt -y install git 
 fi
 
 
 FILE=/usr/bin/whois
-if [ -f "$FILE" ]; then
-    echo "$FILE exists."
+if [ -f "$FILE" ]; then 
+    echo "$FILE exists." | tee -a output.txt
 else 
-    echo "$FILE does not exist."
-    sudo apt install whois
+    echo "$FILE does not exist." | tee -a output.txt
+    sudo apt -y install whois
 fi
 
 FILE=/usr/bin/ipcalc
 if [ -f "$FILE" ]; then
-    echo "$FILE exists."
+    echo "$FILE exists." | tee -a output.txt
 else 
-    echo "$FILE does not exist."
-    sudo apt install ipcalc
+    echo "$FILE does not exist." | tee -a output.txt
+    sudo apt -y install ipcalc
 fi
 
 
 FILE=/usr/bin/nmap
-if [ -f "$FILE" ]; then
-    echo "$FILE exists."
+if [ -f "$FILE" ]; then 
+    echo "$FILE exists." | tee -a output.txt
 else 
-    echo "$FILE does not exist."
-    sudo apt install nmap
+    echo "$FILE does not exist." | tee -a output.txt
+    sudo apt -y install nmap
 fi
 
 
 FILE=/usr/bin/speedtest-cli
 if [ -f "$FILE" ]; then
-    echo "$FILE exists."
+    echo "$FILE exists." | tee -a output.txt
 else 
-    echo "$FILE does not exist."
-    sudo apt install speedtest-cli
+    echo "$FILE does not exist." | tee -a output.txt
+    sudo apt -y install speedtest-cli
 fi
 
 
 FILE=/usr/bin/neofetch
 if [ -f "$FILE" ]; then
-    echo "$FILE exists."
+    echo "$FILE exists." | tee -a output.txt
 else 
-    echo "$FILE does not exist."
-    sudo apt install neofetch
+    echo "$FILE does not exist." | tee -a output.txt
+    sudo apt -y install neofetch
 fi
-
 
 
 FILE=/usr/bin/cmatrix
 if [ -f "$FILE" ]; then
-    echo "$FILE exists."
+    echo "$FILE exists." | tee -a output.txt
 else 
-    echo "$FILE does not exist."
-    sudo apt install cmatrix
+    echo "$FILE does not exist." | tee -a output.txt
+    sudo apt -y install cmatrix
 fi
 
 
 sudo apt -y install curl bind9-host mtr-tiny jq grepcidr ncat aha
+
