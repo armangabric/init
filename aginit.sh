fce6b6b5fd849c81d9bf2c1e5b3c87c47d3c1cd1
Arman G <55468719+armangabric@users.noreply.github.com>
2024-08-27 20:37:17 -0400

    updating logic

--- aginit.sh
+++ aginit.sh
@@ -1,5 +1,17 @@
+#OS Detection - for giggles now but will be necessary later.
+#Run updates 
+#Tools / binaries without necessary validations 
+#Tools / binaries with necessary validations 
+#Tools / binaries that can be installed by their own scripts 
+
+
+
+
 sudo apt-get update && sudo apt-get upgrade
 #not one binary - multiple tools - check for actual binaries first then add the file checks
+
+sudo apt -y install curl bind9-host mtr-tiny jq grepcidr ncat aha net-tools iftop nethogs
+#keep the tools / binaries without validation checks at the start of the script. 
 
 FILE=/usr/bin/ipcalc
 if [ -f "$FILE" ]; then 
@@ -16,9 +28,7 @@ else
     echo "$FILE does not exist." | tee -a output.txt
     sudo apt -y install zsh
 fi
-
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-#check for actual binaries first then add the file checks
+#necessary for Ohmysz.sh
 
 FILE=/usr/bin/git
 if [ -f "$FILE" ]; then 
@@ -105,10 +115,10 @@ else
     echo "$FILE does not exist." | tee -a output.txt
     sudo apt -y install iftop
 fi
+#need to fix this later, validation not working as expected - gona make this more simple for now. 
 
-
-sudo apt -y install curl bind9-host mtr-tiny jq grepcidr ncat aha
-sudo apt -y install net-tools
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+#check for actual binaries first then add the file checks
 
 #echo "Now we will try to install tabby.sh this was not tested but here we go!"
 curl -s https://packagecloud.io/install/repositories/eugeny/tabby/script.deb.sh | sudo bash
